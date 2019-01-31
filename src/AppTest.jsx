1d42f7183ad49f098387a4d7bf95e1b05fb165a0
joshpitzalis <joshpitzalis@gmail.com>
2019-01-31 18:47:35 +0530

    table with freelancer details now works

--- src/AppTest.jsx
+++ src/AppTest.jsx
@@ -1,18 +1,9 @@
+import { Avatar, Table, Tag } from 'antd';
 import React from 'react';
 import { componentFromStream, createEventHandler } from 'recompose';
-import { combineLatest, merge, of } from 'rxjs';
+import { combineLatest, of } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
-import {
-  catchError,
-  debounceTime,
-  delay,
-  filter,
-  map,
-  pluck,
-  startWith,
-  switchMap,
-  tap
-} from 'rxjs/operators';
+import { catchError, map, pluck, startWith } from 'rxjs/operators';
 
 const App = componentFromStream(prop$ => {
   const { handler, stream } = createEventHandler();
@@ -24,62 +15,77 @@ const App = componentFromStream(prop$ => {
   return combineLatest(prop$, clickStream$).pipe(
     map(([props, value]) => (
       <div>
-        <input onChange={handler} placeholder="GitHub username" />
-        <User user={value} />
-        <Table />
+        <TableData />
       </div>
     ))
   );
 });
 
-const User = componentFromStream(prop$ => {
-  const loading$ = of(<h3>Loading...</h3>);
-  const formatUrl = user => `https://api.github.com/users/${user}`;
-  const getUser$ = prop$.pipe(
-    debounceTime(500),
-    pluck('user'),
-    filter(user => user && user.length),
-    map(formatUrl),
-    switchMap(url =>
-      merge(
-        loading$,
-        ajax(url).pipe(
-          pluck('response'),
-          delay(1500),
-          map(({ avatar_url, name, email, hireable }) => (
-            <ul>
-              <img src={avatar_url} height="100" width="100" />
-              <li>{name}</li>
-              <li>{email}</li>
-              <li>{hireable ? `✅ ` : `❌`}</li>
-            </ul>
-          )),
-          catchError(error => of(<Error {...error} />))
-        )
-      )
-    )
-  );
-  return getUser$;
-});
+const TableData = componentFromStream(prop$ => {
+  const url = `https://www.freelancer.com/api/users/0.1/users/directory?jobs=[1]&&avatar=true&&portfolio_details=true`;
 
-const Table = componentFromStream(prop$ => {
-  const url = `https://api.github.com/users?since=135`;
+  const columns = [
+    {
+      title: '',
+      dataIndex: 'avatar',
+      key: 'avatar',
+      render: url => <Avatar src={`https://www.freelancer.in/${url}`} />
+    },
+    {
+      title: 'Name',
+      dataIndex: 'username',
+      key: 'name',
+      render: text => <a href="javascript:;">{text}</a>
+    },
+    {
+      title: 'Hourly Rate',
+      dataIndex: 'hourly_rate',
+      key: 'hourly_rate',
+      render: rate => <p>${rate}/hour</p>
+    },
+    {
+      title: 'Location',
+      dataIndex: 'location.country.name',
+      key: 'location'
+    },
+    {
+      title: 'Jobs Completed',
+      dataIndex: 'portfolio_count',
+      key: 'portfolio_count'
+    },
+
+    {
+      title: 'Categories',
+      key: 'tags',
+      dataIndex: 'jobs',
+      render: jobs => {
+        const jobTags = jobs.map(job => job.category.name);
+        const uniqueArray = [...new Set(jobTags)];
+        return (
+          <span>
+            {uniqueArray.map(tag => (
+              <Tag color="green" key={tag}>
+                {tag.toUpperCase()}
+              </Tag>
+            ))}
+          </span>
+        );
+      }
+    },
+    {
+      title: 'Hire',
+      dataIndex: 'username',
+      key: 'hire',
+      render: username => (
+        <a href={`https://www.freelancer.in/u/${username}`}>Hire</a>
+      )
+    }
+  ];
 
   return ajax(url).pipe(
     pluck('response'),
-    tap(console.warn),
-    map(users => (
-      <ul>
-        {users.map(({ avatar_url, name, email, hireable }) => (
-          <li>
-            <img src={avatar_url} height="100" width="100" />
-            <p>{name}</p>
-            <p>{email}</p>
-            <p>{hireable ? `✅ ` : `❌`}</p>
-          </li>
-        ))}
-      </ul>
-    ))
+    map(data => <Table columns={columns} dataSource={data.result.users} />),
+    catchError(error => of(<Error {...error} />))
   );
 });
 
